4fb21da5d3c9f714864cc94cd4ac269e382ab670
MCole-K9 <m.b.cole300@gmail.com>
2023-07-11 23:15:36 -0500

    login logic

--- src/lib/schemas/auth-schemas.ts
+++ src/lib/schemas/auth-schemas.ts
@@ -4,7 +4,7 @@ import {z} from 'zod';
 export const email_registration_schema = z.object({
     first_name: z.string().min(1, {message: "First Name is Required"}).max(255),
     last_name: z.string().min(1, {message: "Last Name is Required"}).max(255),
-    email: z.string().email({message: "Invalid Email"}),
+    email: z.string().email({message: "Invalid Email"}).toLowerCase(),
     password: z.string().min(8, {message: "Password Should Be a Minimum of 8 Characters"}).max(255),
     password_confirmation: z.string().min(8, {message: "Password Should Be a Minimum of 8 Characters"}).max(255),
     user_role: z.enum([Role.ORGANIZATION_REPRESENTATIVE, Role.CANDIDATE]).default("CANDIDATE")
@@ -17,4 +17,10 @@ export const email_registration_schema = z.object({
             path: ["password_confirmation"]
         })
     }
+})
+
+
+export const email_login_schema = z.object({
+    email: z.string().email({message: "Email Format Is Incorrect"}).toLowerCase(),
+    password: z.string(),
 })
