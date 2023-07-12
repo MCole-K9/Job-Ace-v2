ced814751c2a052fc74acc21e44ceef72af6749a
MCole-K9 <m.b.cole300@gmail.com>
2023-07-11 23:23:23 -0500

    logout logic

---
+++ src/routes/(auth)/logout/+server.ts
@@ -0,0 +1,15 @@
+import { error, redirect } from '@sveltejs/kit';
+import type { RequestHandler } from './$types';
+
+export const POST: RequestHandler = async ({locals}) => {
+
+    const {error: err} = await locals.supabase.auth.signOut();
+
+    if (err) {
+        
+        throw error(500, err.message); // change message
+    }
+
+    throw redirect(301, '/'); 
+    
+};
