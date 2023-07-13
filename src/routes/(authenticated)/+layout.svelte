3eddad57c792ba6303d2a1c27e00c09dddcdeb22
MCole-K9 <m.b.cole300@gmail.com>
2023-07-12 21:54:01 -0500

    Avatar Menu

--- src/routes/(authenticated)/+layout.svelte
+++ src/routes/(authenticated)/+layout.svelte
@@ -1,36 +1,67 @@
 <script lang="ts">
-    import type { LayoutData } from './$types';
-    import { page } from '$app/stores';
+	import type { LayoutData } from './$types';
+	import { page } from '$app/stores';
 	import { sineIn } from 'svelte/easing';
-	import { Button, Drawer } from 'flowbite-svelte';
-    import Sidebar from './Sidebar.svelte';
-    let hidden2 = false;
-     
-    let transitionParams = {
-        x: -320,
-        duration: 200,
-        easing: sineIn
-    };
+	import { Avatar, Button, Drawer, DropdownHeader } from 'flowbite-svelte';
+	import Sidebar from './Sidebar.svelte';
+	import {
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger,
+		Dropdown,
+		DropdownItem,
+		Chevron,
+		DropdownDivider
+	} from 'flowbite-svelte';
 
-    export let data: LayoutData;
+	let hidden = true;
 
-	
-    
-    
+	let transitionParams = {
+		x: -320,
+		duration: 200,
+		easing: sineIn
+	};
+
+	export let data: LayoutData;
 </script>
 
-<div class="text-center">
-  <Button color="red" on:click={() => (hidden2 = false)}>Show navigation</Button>
-</div>
-<Drawer transitionType="fly" {transitionParams} bind:hidden={hidden2} id='m-sidebar'>
-    <div class='flex items-center'>     
-        <Sidebar />
-    </div>
+<Navbar let:hidden let:toggle>
+	<NavBrand href="/">
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+			Job Ace
+		</span>
+	</NavBrand>
+
+    <!-- Avatar Menu Component -->
+	<div class="flex items-center md:order-2">
+		<Avatar id="avatar-menu" alt="MC" />
+	</div>
+	<Dropdown placement="bottom" triggeredBy="#avatar-menu">
+		<DropdownHeader>
+			<span class="block text-sm"> Bonnie Green </span>
+			<span class="block truncate text-sm font-medium"> name@flowbite.com </span>
+		</DropdownHeader>
+		<DropdownItem>Dashboard</DropdownItem>
+		<DropdownItem>Settings</DropdownItem>
+		<DropdownItem>Earnings</DropdownItem>
+		<DropdownDivider />
+		<DropdownItem>Sign out</DropdownItem>
+	</Dropdown>
+    <!-- Avatar Menu Component  -->
+</Navbar>
+<Drawer transitionType="fly" {transitionParams} bind:hidden={hidden} id="m-sidebar">
+	<div class="flex items-center">
+		<Sidebar />
+	</div>
 </Drawer>
 <div class="flex flex-row min-h-screen">
-    <aside class="sidebar w-64 md:shadow transform -translate-x-full md:translate-x-0 transition-transform duration-150 ease-in">
-        <Sidebar />
-    </aside>
+	<aside
+		class="sidebar w-64 md:shadow transform -translate-x-full md:translate-x-0 transition-transform duration-150 ease-in"
+	>
+		<Sidebar />
+	</aside>
 	<main class="flex flex-col flex-grow -ml-64 md:ml-0 transition-all duration-150 ease-in p-6">
 		<slot />
 	</main>
