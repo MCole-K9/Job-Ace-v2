8cd022c89f760b0799e7d930460e98ab0da27dfe
khalid sheriff <khalalimsheriff@gmail.com>
2023-07-14 05:49:50 -0500

    filled out function for all roles but orgRep (untested) pending further discussion

--- src/routes/api/admin/users/+server.ts
+++ src/routes/api/admin/users/+server.ts
@@ -64,12 +64,6 @@ export async function POST({request, locals:{getSession, supabase}}){
                                 }
                             })
                         }
-                        
-
-
-                        // now the job is to create the relevant profile role etc. etc.
-
-                        console.log(data);
 
                         if (error){
                             return json(error,  {status: 500,
@@ -85,36 +79,187 @@ export async function POST({request, locals:{getSession, supabase}}){
                         return json({result: adminValidator.error}, {status: 500, 
                             statusText: "Unable to create account"});
                     }
+            
                 case Role.SUPPORT:
-                    const supportValidator = adminOrSupportOrUser;
-                    supportValidator.parse(userData);
+                    const supportValidator = adminOrSupportOrUser.safeParse(userData);
 
-                    if (supportValidator){
-                        return json({}, {status: 201,
-                            statusText: "Support Staff account successfully created"});
+                    if (supportValidator.success){
+                       
+                        const { data, error } = await adminClient.createUser({
+                            email: supportValidator.data.email,
+                            password: supportValidator.data.password,
+                            email_confirm: true
+                        })
+
+                        let supportProfile;
+
+                        if (data.user){
+                            const prisma = new PrismaClient();
+                            supportProfile = await prisma.profile.create({
+                                data: {
+                                    user_role: Role.SUPPORT,
+                                    users: {
+                                        connect: {
+                                            id: data.user.id
+                                        }
+                                    }
+                                }
+                            })
+                        }
+
+                        if (error){
+                            return json(error,  {status: 500,
+                            statusText: "Unable to create Account"});
+
+                        }
+                        else {
+                            return json({ userData: data, profileData: supportProfile}, {status: 201,
+                                statusText: "Support Staff account successfully created"});
+                        }
                     }
                     else {
-                        // return the relevant errors
-                    }    
+                        return json({result: supportValidator.error}, {status: 500, 
+                            statusText: "Unable to create account"});
+                    }
+                
                 case Role.ORGANIZATION_REPRESENTATIVE:
                     
                     return json({}, {status: 201,
                         statusText: "Organization Representative account successfully created"});
+                
                 case Role.CAREER_COACH:
 
-                    return json({}, {status: 201,
-                        statusText: "Career Coach successfully created"});
+                    const coachValidator = candidateOrCoach.safeParse(userData);
+
+                    if (coachValidator.success){
+                    
+                        const { data, error } = await adminClient.createUser({
+                            email: coachValidator.data.email,
+                            password: coachValidator.data.password,
+                            email_confirm: true
+                        })
+
+                        let coachProfile;
+
+                        if (data.user){
+                            const prisma = new PrismaClient();
+                            coachProfile = await prisma.profile.create({
+                                data: {
+                                    user_role: Role.CANDIDATE,
+                                    first_name: coachValidator.data.firstName,
+                                    last_name: coachValidator.data.lastName,
+                                    users: {
+                                        connect: {
+                                            id: data.user.id
+                                        }
+                                    }
+                                }
+                            })
+                        }
+
+                        if (error){
+                            return json(error,  {status: 500,
+                            statusText: "Unable to create Account"});
+
+                        }
+                        else {
+                            return json({ userData: data, profileData: coachProfile}, {status: 201,
+                                statusText: "Career Coach account successfully created"});
+                        }
+                    }
+                    else {
+                        return json({result: coachValidator.error}, {status: 500, 
+                            statusText: "Unable to create account"});
+                    }
+                
                 case Role.CANDIDATE:
 
-                    return json({}, {status: 201, 
-                    statusText: "Candidate Account Successfully created"});
+                    const candidateValidator = candidateOrCoach.safeParse(userData);
+
+                    if (candidateValidator.success){
+                    
+                        const { data, error } = await adminClient.createUser({
+                            email: candidateValidator.data.email,
+                            password: candidateValidator.data.password,
+                            email_confirm: true
+                        })
+
+                        let candidateProfile;
+
+                        if (data.user){
+                            const prisma = new PrismaClient();
+                            candidateProfile = await prisma.profile.create({
+                                data: {
+                                    user_role: Role.CANDIDATE,
+                                    first_name: candidateValidator.data.firstName,
+                                    last_name: candidateValidator.data.lastName,
+                                    users: {
+                                        connect: {
+                                            id: data.user.id
+                                        }
+                                    }
+                                }
+                            })
+                        }
+
+                        if (error){
+                            return json(error,  {status: 500,
+                            statusText: "Unable to create Account"});
+
+                        }
+                        else {
+                            return json({ userData: data, profileData: candidateProfile}, {status: 201,
+                                statusText: "Candidate account successfully created"});
+                        }
+                    }
+                    else {
+                        return json({result: candidateValidator.error}, {status: 500, 
+                            statusText: "Unable to create account"});
+                    }
+                
                 case Role.USER:
                     
-                    return json({}, {status: 201, 
-                        statusText: "Generic User account successfully created"});
-            }
+                    const userValidator = adminOrSupportOrUser.safeParse(userData);
 
+                    if (userValidator.success){
+                    
+                        const { data, error } = await adminClient.createUser({
+                            email: userValidator.data.email,
+                            password: userValidator.data.password,
+                            email_confirm: true
+                        })
 
+                        let userProfile;
+
+                        if (data.user){
+                            const prisma = new PrismaClient();
+                            userProfile = await prisma.profile.create({
+                                data: {
+                                    user_role: Role.USER,
+                                    users: {
+                                        connect: {
+                                            id: data.user.id
+                                        }
+                                    }
+                                }
+                            })
+                        }
+
+                        if (error){
+                            return json(error,  {status: 500,
+                            statusText: "Unable to create Account"});
+
+                        }
+                        else {
+                            return json({ userData: data, profileData: userProfile}, {status: 201,
+                                statusText: "User account successfully created"});
+                        }
+                    }
+                    else {
+                        return json({result: userValidator.error}, {status: 500, 
+                            statusText: "Unable to create account"});
+                    }
+                }
         }
         else {
             throw error(403, "User is not authorized to perform action");
