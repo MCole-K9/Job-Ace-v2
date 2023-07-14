55504888d3dd38961a404ef38c3724815035164d
MCole-K9 <m.b.cole300@gmail.com>
2023-07-14 00:38:45 -0500

    extracted the logic for rendering sidebar links

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,5 +1,6 @@
 
 // Need to tidy up this type, i.e separate the parent and child types
+//consider adding role permissions to the type
 export type Link = {
     href?: string ;
     label: string;
